25bb6bb761a4f027978e657e55c0861db5bcd630
Breanna Henriquez <breanna4179@gmail.com>
2024-04-29 22:41:01 -0400

    Worked on normalized points
    Made it so when the user clicks anywhere on the image the coordinate and normalized coordinate are displayed on screen.
    
    Currently the origin point of the image is set to the top left of the image.
    
    Added a dropbox field so that the user can pick where the origin point is located. For now there will only be two options, Top-Left and Center.

--- Normalize Point Picker/index.js
+++ Normalize Point Picker/index.js
@@ -2,21 +2,27 @@ window.onload = function(){
     let input = document.querySelector("#fileInput");
     let output = document.querySelector("#fileOutput");
     let outputHolder = document.querySelector("#output-holder")
-    
-    console.log(input);
+
+    const orignalWidth = outputHolder.getBoundingClientRect().width;
+    const orignalHeight = outputHolder.getBoundingClientRect().height;
+
+    let xPosition = document.querySelector("#x-position");
+    let yPosition = document.querySelector("#y-position");
+
+    let xNormalizedPosition = document.querySelector("#x-normalizedPosition");
+    let yNormalizedPosition = document.querySelector("#y-normalizedPosition");
+
+    let dropDown = document.querySelector("#origins");
 
     input.addEventListener("change", function(e){
-        console.log("here");
         output.src = URL.createObjectURL(e.target.files[0]);
 
         output.onload = function()
         {
-           console.log(outputHolder.getBoundingClientRect().width);
-           let parentWidth = outputHolder.getBoundingClientRect().width;
-           let parentHeight = outputHolder.getBoundingClientRect().height;
+           let parentWidth = orignalWidth;
+           let parentHeight = orignalHeight;
 
            let aspectRatio = output.naturalWidth / output.naturalHeight;
-           console.log(aspectRatio);
 
            let newWidth = parentWidth;
            let newHeight = parentWidth / aspectRatio;
@@ -28,7 +34,37 @@ window.onload = function(){
 
            output.style.width = newWidth + "px";
            output.style.height = newHeight + "px";
+
+           outputHolder.style.height = newHeight + "px";
         }
     })
+
+    output.addEventListener("click", function(e){
+        let rect = output.getBoundingClientRect();
+
+        SetCoordinateUsingTopLeftOrigins(e.clientX, e.clientY, rect);
+    })
+
+    function SetCoordinateUsingTopLeftOrigins(x,y, rect){
+        let xPos = GetRoundedNumber(x - rect.left);
+        let yPos = GetRoundedNumber(y - rect.top);
+
+        let xNormPos = GetRoundedNumber((x - rect.left) / rect.width);
+        let yNormPos = GetRoundedNumber((y - rect.top) / rect.height);
+
+        SetTextElements(xPos, yPos, xNormPos, yNormPos);
+    }
+
+    function GetRoundedNumber(num)
+    {
+        return Math.round(((num) + Number.EPSILON) * 100) / 100;
+    }
+
+    function SetTextElements(x, y, xNormalized, yNormalized){
+        xPosition.innerHTML = "X: " + x;
+        yPosition.innerHTML = "Y: " + y;
+        xNormalizedPosition.innerHTML = "X Normalized: " + xNormalized;
+        yNormalizedPosition.innerHTML = "Y Normalized: " + yNormalized;
+    }
 };
 
